c26c141d1ec7e6bcf45b3aa37ddbd7cdf27121af
Xingcheng Lin <xclin@xclins-imac.local>
2024-05-17 20:54:03 -0400

    cleaned up

--- training/optimization/for_bindingE/1nkp/sequences/cmd.cleanSequences.sh
+++
@@ -1,9 +0,0 @@
-export seqFile=$1
-
-gsed -i "/>/d; /structureX/d" $seqFile
-tr -d '\n' < $seqFile  > a
-tr "*" "\n" < a > b
-mv b $seqFile
-# Get rid of the chain separating line;
-gsed -i "s/\///g" $seqFile
-
--- training/optimization/for_training_gamma/sequences/cmd.cleanSequences.sh
+++
@@ -1,9 +0,0 @@
-export seqFile=$1
-
-sed -i '' "/>/d; /structureX/d" $seqFile
-tr -d '\n' < $seqFile  > a
-tr "*" "\n" < a > b
-mv b $seqFile
-# Get rid of the chain separating line;
-sed -i '' "s/\///g" $seqFile
-
