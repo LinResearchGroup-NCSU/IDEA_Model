f2f07efddc807bc2596b1fb60b15fe10abd5f014
YAFANZ <142270316+YAFANZ@users.noreply.github.com>
2024-11-11 18:23:23 -0500

    Update buildseq.py

--- 3max_case/optimization/for_bindingE/template/sequences/buildseq.py
+++ 3max_case/optimization/for_bindingE/template/sequences/buildseq.py
@@ -1,11 +1,19 @@
-# This script will get argument passed by command line;
+# This script will get the argument passed by command line
 import sys
-from modeller import *
-# Get the sequence of the PDB file, and write to an alignment file
+from modeller import Environ, Model, Alignment
+
+# Get the sequence of the PDB file and write to an alignment file
 code = sys.argv[1]
 
-e = environ()
-m = model(e, file=code)
-aln = alignment(e)
-aln.append_model(m, align_codes=code)
-aln.write(file=code + '.seq')
+# Initialize Modeller environment
+env = Environ()
+
+# Load the model from the PDB file
+mdl = Model(env, file=code)
+
+# Create an alignment object and add the model
+aln = Alignment(env)
+aln.append_model(mdl, align_codes=code)
+
+# Write the sequence to an alignment file
+aln.write(file=f"{code}.seq")
